b642a34df103330b886e7335ea981f3c955cbc79
GiannfrancoCrovetto <gcrovetto2021@fau.edu>
2025-04-02 12:32:19 -0400

    Added code for the clock

---
+++ script.js
@@ -0,0 +1,34 @@
+const clockDisplay = document.getElementById('clock');
+const dateDisplay = document.getElementById('date');
+const TIME_INTERVAL_UPDATE = 1000;
+
+function formatTime(hours, minutes, seconds) {
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    return '${hours}:${minutes}:${seconds}';
+}
+
+function updateClock() {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    clockDisplay.textContent = formatTime(hours, minutes, seconds);
+}
+
+function updateDate() {
+    let now = new Date();
+    let day = now.toDateString();
+    dateDisplay.textContent = day;
+}
+
+function init() {
+    updateClock();
+    setInterval(updateClock, TIME_UPDATE_INTERVAL);
+    if (dateDisplay) {
+        updateDate();
+    }
+}
+
+init();
